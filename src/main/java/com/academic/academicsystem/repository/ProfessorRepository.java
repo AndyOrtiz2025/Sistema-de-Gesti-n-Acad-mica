e027fea4fe759e2712480c21bcaedb150e309a60
Andyortiz2025 <andyortiz743@gmail.com>
2025-09-03 19:54:16 -0600

    Implementando una API de Reportes con Spring Boot

--- src/main/java/com/academic/academicsystem/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.academic.academicsystem.repository;
-
-import com.academic.academicsystem.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
-  List<Professor> findByFullnameContainingIgnoreCase(String fullname);
-}
